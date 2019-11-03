c4d6b6da0cf7149f8c5a2c45f5bb671ccca10f97
1@q.q <1@q.q>
2019-11-03 22:32:45 +0300

    добавил Энкодер, но оказалось неудобно с ним работать

--- avr/solderstatin/keyboard.c
+++ avr/solderstatin/keyboard.c
@@ -16,6 +16,31 @@
 #define ButtonIsOff(portOut, pin)		(*PIN(&portOut) & (1<<pin))
 #define ButtonIsOn(portOut, pin)		(!((*PIN(&portOut)) & (1<<pin)))
 
+//
+#define ENCODER_A_BIT					0		//статус фазы A энкодера
+#define ENCODER_B_BIT					1		//статус фазы B
+#define ENCODER_COUNT_BIT				2		//Два бита на полный статус
+#define EncoderCurrentState(phaseAport, phaseApin, phaseBport, phaseBpin)	((ButtonIsOn(phaseAport, phaseApin)?(1<<ENCODER_A_BIT):(0<<ENCODER_A_BIT)) | (ButtonIsOn(phaseBport, phaseBpin)?(1<<ENCODER_B_BIT):(0<<ENCODER_B_BIT)))
+#define ENCODER_ERROR					3
+const int8_t EncoderStates[] = { //check ENCODER_A_BIT & ENCODER_B_BIT !!!
+	0,				//0000
+	-1,				//0001
+	1,				//0010
+	ENCODER_ERROR,	//0011
+	1,				//0100
+	0,				//0101
+	ENCODER_ERROR,	//0110
+	-1,				//0111
+	-1,				//1000
+	ENCODER_ERROR,	//1001
+	0,				//1010
+	1,				//1011
+	ENCODER_ERROR,	//1100
+	1,				//1101
+	-1,				//1110
+	0				//1111
+};
+
 ISR(SOLDER_BUTTON_INT){
 	if ((solder.state != STATE_OFF) && (!solder.delayOff)){
 		SolderOff();
@@ -63,8 +88,45 @@ void keyboardRepeat(void){
 	repeatMode(&fan_head);
 	SetTimerTask(keyboardRepeat, KEY_PERIOD_REPEAT_MS);
 }
-
+/*
 inline void keyProcess(sDevice *device, u08 onOff, u08 plus, u08 minus){
+if (device->delayOff){
+device->delayOff--;
+}
+if (onOff){
+if ((device->state == STATE_OFF) && (!device->periodRepeatMs)){
+device->state = STATE_ON;
+device->delayOff = KEY_DELAY_REPEAT_TICK;
+}
+}
+else{
+if (plus || minus){
+device->periodSettingS = SETTING_MODE_PERIOD_S;
+if (device->state == STATE_ON){
+device->setSelect = device->need;
+device->state = STATE_SET;
+device->periodRepeatMs = KEY_DELAY_REPEAT_TICK;
+}
+else if (device->state == STATE_SET){
+if (!device->periodRepeatMs){
+if (plus && ((device->setSelect + KEY_STEP_CHANGE) <= device->maxValue)){
+device->setSelect += KEY_STEP_CHANGE;
+}
+else if (minus && device->setSelect){
+device->setSelect -= (device->setSelect > KEY_STEP_CHANGE)?KEY_STEP_CHANGE:device->setSelect;
+}
+device->periodRepeatMs = KEY_DELAY_REPEAT_TICK;
+}
+}
+}
+if (!plus && !minus){
+device->periodRepeatMs = 0;
+}
+}
+}
+*/
+inline void keyProcess(sDevice *device, uint8_t onOff, uint8_t currEnc){
+
 	if (device->delayOff){
 		device->delayOff--;
 	}
@@ -75,27 +137,20 @@ inline void keyProcess(sDevice *device, u08 onOff, u08 plus, u08 minus){
 		}
 	}
 	else{
-		if (plus || minus){
+		int8_t	currentEncoder = EncoderStates[(device->encoderState << ENCODER_COUNT_BIT) | currEnc];
+		uint8_t tmp = device->encoderState;
+		device->encoderState = currEnc;
+		if (currentEncoder && (currentEncoder != ENCODER_ERROR)){
+			fan_head.current = (tmp << ENCODER_COUNT_BIT) | currEnc;
 			device->periodSettingS = SETTING_MODE_PERIOD_S;
 			if (device->state == STATE_ON){
 				device->setSelect = device->need;
 				device->state = STATE_SET;
-				device->periodRepeatMs = KEY_DELAY_REPEAT_TICK;
 			}
 			else if (device->state == STATE_SET){
-				if (!device->periodRepeatMs){
-					if (plus && ((device->setSelect + KEY_STEP_CHANGE) <= device->maxValue)){
-						device->setSelect += KEY_STEP_CHANGE;
-					}
-					else if (minus && device->setSelect){
-						device->setSelect -= (device->setSelect > KEY_STEP_CHANGE)?KEY_STEP_CHANGE:device->setSelect;
-					}
-					device->periodRepeatMs = KEY_DELAY_REPEAT_TICK;
-				}
+				if ((currentEncoder == 1) && ((device->setSelect+KEY_STEP_CHANGE) <= device->maxValue)) device->setSelect += KEY_STEP_CHANGE;
+				if ((currentEncoder == -1) && (device->setSelect >= KEY_STEP_CHANGE)) device->setSelect -= KEY_STEP_CHANGE;
 			}
-		}
-		if (!plus && !minus){
-			device->periodRepeatMs = 0;
 		}
 	}
 }
@@ -103,15 +158,14 @@ inline void keyProcess(sDevice *device, u08 onOff, u08 plus, u08 minus){
 void keyboard(void){
 	keyProcess(&solder,
 	ButtonIsOn(SOLDER_BUTTON_ON_OUT, SOLDER_BUTTON_ON_PIN),
-	ButtonIsOn(SOLDER_BUTTON_PLUS_OUT, SOLDER_BUTTON_PLUS_PIN),
-	ButtonIsOn(SOLDER_BUTTON_MINUS_OUT, SOLDER_BUTTON_MINUS_PIN)
+	EncoderCurrentState(SOLDER_BUTTON_PLUS_OUT, SOLDER_BUTTON_PLUS_PIN, SOLDER_BUTTON_MINUS_OUT, SOLDER_BUTTON_MINUS_PIN)
 	);
-	keyProcess(&fan_head,
+	/*keyProcess(&fan_head,
 	ButtonIsOn(FAN_BUTTON_ON_OUT, FAN_BUTTON_ON_PIN),
 	ButtonIsOn(FAN_BUTTON_PLUS_OUT, FAN_BUTTON_PLUS_PIN),
 	ButtonIsOn(FAN_BUTTON_MINUS_OUT, FAN_BUTTON_MINUS_PIN)
-	);
-	SetTimerTask(keyboard, KEY_SCAN_PERIOD_MS);
+	);*/
+	SetTimerTask(keyboard, 10/*KEY_SCAN_PERIOD_MS*/);
 }
 
 
@@ -125,6 +179,8 @@ void keyboard_init(void){
 	PinInputMode(FAN_BUTTON_PLUS_OUT, FAN_BUTTON_PLUS_PIN);
 	
 	solder.maxValue = SOLDER_MAX;
+	//Начальное значение ног энкодера паяльника
+	solder.encoderState = EncoderCurrentState(SOLDER_BUTTON_PLUS_OUT, SOLDER_BUTTON_PLUS_PIN, SOLDER_BUTTON_MINUS_OUT, SOLDER_BUTTON_MINUS_PIN);
 	fan_head.maxValue = FAN_MAX;
 
 	SolderButInteruptOn();
